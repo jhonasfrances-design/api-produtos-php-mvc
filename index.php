40d2c8f10f20fbca5b4f934cf97f4fec893d79f5
thayslcorrea <thays.lacerdac@gmail.com>
2026-05-18 09:16:09 -0300

    fix: css

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
+<?php 
+    define('BASE_URL', 'http://localhost/API-REST-PRODUTOS-PHP');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <title>Simple Framework</title>
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css"/>
   </head>
   <body>
 
   <?php
     require 'api/autoload.php';
-    define('BASE_URL', 'http://localhost/API-REST-PRODUTOS-PHP');
     
     use api\core\App;
     use api\core\Controller;
